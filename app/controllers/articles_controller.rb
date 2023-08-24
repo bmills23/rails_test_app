a51621465b3f40df701199e243e20883eccfd902
Bryan Mills <millsbryang@gmail.com>
2023-08-24 06:54:58 -0600

    Added article editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,37 @@ class ArticlesController < ApplicationController
     end    
 
     def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
-        render plain: params[:article]
+        ## need to whitelist @article = Article.new(params[:title, :description])
+        ## we need to require the top level key of article and permit the keys
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        
+    
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to article_path(@article)
+        else
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated successfully."
+            redirect_to @article
+        else 
+            render :edit, status: :unprocessable_entity
+        end
+
     end
 
 end

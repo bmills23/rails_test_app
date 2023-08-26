2021e0e51cf0981a28681550b84fbd5df34472e5
Bryan Mills <millsbryang@gmail.com>
2023-08-26 17:52:27 -0600

    DRYed code with private methods in article controllers and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+    ## runs before the actions in each route are enacted
+    before_action :set_article, only: [:show, :edit, :update, :destroy] 
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -13,13 +15,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
         ## need to whitelist @article = Article.new(params[:title, :description])
         ## we need to require the top level key of article and permit the keys
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         
     
         if @article.save
@@ -32,8 +33,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article updated successfully."
             redirect_to @article
         else 
@@ -43,9 +43,20 @@ class ArticlesController < ApplicationController
     end
 
     def destroy 
-        @article = Article.find(params[:id])
         @article.destroy 
         redirect_to articles_path
     end
-    
+
+    ## anything below private is a private method, just a keyword to isolate 
+    ## the methods to this controller, i.e. not accessible elsewhere
+    private 
+
+    def set_article 
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
